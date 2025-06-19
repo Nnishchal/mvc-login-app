6b5aa46994166998952fcbbaaac38fa9bf839d71
Nnishchal <nnishchal@algomau.ca>
2025-06-19 13:13:54 -0400

    create register.php

---
+++ my-mvc-app/views/register.php
@@ -0,0 +1,7 @@
+<link rel="stylesheet" href="css/style.css">
+<h2>Register</h2>
+<form method="post">
+    Username: <input name="username" required><br>
+    Password: <input type="password" name="password" required><br>
+    <button type="submit">Register</button>
+</form>
