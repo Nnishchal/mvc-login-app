1282460ca1c284e28f7f044225bd62ffc47d0e81
Nnishchal <nnishchal@algomau.ca>
2025-06-19 13:13:35 -0400

    create login.php

---
+++ my-mvc-app/views/login.php
@@ -0,0 +1,8 @@
+<link rel="stylesheet" href="css/style.css">
+<h2>Login</h2>
+<form method="post">
+    Username: <input name="username" required><br>
+    Password: <input type="password" name="password" required><br>
+    <button type="submit">Login</button>
+</form>
+<p><a href="index.php?page=create">Create Account</a></p>
